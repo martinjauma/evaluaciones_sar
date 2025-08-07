226a30846695f600564eb73a4241770ecb9c2eba
martinjauma <martin.jauma@gmail.com>
2025-08-07 14:00:56 -0300

    modificar ignore

--- config_mongo.py
+++
@@ -1,8 +0,0 @@
-# config_mongo.py
-
-# Cadena de conexión a MongoDB (esto puede ser MongoDB local o MongoDB Atlas)
-MONGO_URI = "mongodb+srv://martinjauma:sarAcademia@academiasar.pgsk3mr.mongodb.net/"
-
-# Nombre de la base de datos y la colección donde guardaremos las evaluaciones
-DB_NAME = "SAR"
-COLLECTION_NAME = "evaluaciones_data"
